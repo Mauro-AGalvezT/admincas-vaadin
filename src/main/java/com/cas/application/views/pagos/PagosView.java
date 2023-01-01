7ea4eec9d502e7d2c73c4ea7f1027b84cb4d1b50
start.vaadin.com <no-reply@vaadin.com>
2023-01-01 18:31:30 +0000

    Generated project

---
+++ src/main/java/com/cas/application/views/pagos/PagosView.java
@@ -0,0 +1,31 @@
+package com.cas.application.views.pagos;
+
+import com.cas.application.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Pagos")
+@Route(value = "Pagos", layout = MainLayout.class)
+public class PagosView extends VerticalLayout {
+
+    public PagosView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
